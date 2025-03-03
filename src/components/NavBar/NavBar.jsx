aa096da01402527e01b1da779024768b5c66f1b8
WalterGuerra <wsteven00@gmail.com>
2025-03-03 18:38:58 -0500

    change background color for nav bar and home, as well as add animation to home page for text and image. change accent colors

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,14 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
 import styles from './NavBar.module.css'
+import profilePic from '../../assets/profilePic.jpeg'
 
 const NavBar = () => {
   return (
     <nav className={styles.navContainer}>
       <div className={styles.nameContainer}>
-        Waler Guerra
+        <img src={profilePic} alt="" className={styles.profileImg}/>
+        Walter Guerra
       </div>
       <div className={styles.linksContainer}>
         <ul>Portfolio</ul>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,48 @@
-// css
+// npm modules
+import { motion } from 'framer-motion'
 import styles from './Home.module.css'
-
-const Home = ({ user }) => {
+import xWing from '../../assets/xwing-Fighter.png'
+import xWingFlipped from '../../assets/xwing-Fighter-Flipped.png'
+const Home = () => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <motion.img 
+        src={xWing} 
+        alt="X-Wing Fighter"
+        className={styles.xWing}
+        initial={{ x: '100vw', opacity: 1 }}
+        animate={{ x: '-100vw', opacity: 0 }}
+        transition={{ duration: 5.5, ease: 'easeInOut' }}
+      />
+      <motion.h1 
+        className={styles.name}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 3, duration: 1.5 }}
+      >
+        Hello I'm Walter 👋
+      </motion.h1>
+
+      <motion.img 
+        src={xWingFlipped} 
+        alt="X-Wing Fighter"
+        className={styles.xWing}
+        initial={{ x: '-100vw', opacity: 0 }}
+        animate={{ x: '100vw', opacity: 1 }}
+        transition={{ delay: 3.5, duration: 5.5, ease: 'easeInOut' }}
+      />
+
+      <motion.h2 
+        className={styles.summary}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 7, duration: 1.5 }}
+      >
+        I'm a passionate <span> Software Engineer </span> from New York
+      </motion.h2>
     </main>
   )
 }
 
 export default Home
+
